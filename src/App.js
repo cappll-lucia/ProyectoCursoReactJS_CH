793fcf85d5f5f93f24f24cef55a9de06603db493
lucia cappll <luciacappll@gmail.com>
2022-08-30 15:00:55 -0300

    some attempts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import './App.css';
+
 function App() {
+let nroClase=3;
+
   return (
     <div className="App">
+      {/*comentatio*/}
+      <p style={
+        {
+          color:"salmon",
+          fontWeight: '800',
+          fontSize: '3rem',
+        }
+      }>Hola numero {nroClase}</p>
+      <input placeholder='ingrese su nombre'/>
     </div>
   );
 }
