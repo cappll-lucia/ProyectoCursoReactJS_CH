12061b02ee14c28e1b4b6304ab4d99599cc51324
lucia cappll <luciacappll@gmail.com>
2022-08-30 12:13:33 -0300

    cleanliness default files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+<<<<<<< HEAD
 import './App.css';
+=======
+>>>>>>> 9f2668
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9f2668
     </div>
   );
 }
