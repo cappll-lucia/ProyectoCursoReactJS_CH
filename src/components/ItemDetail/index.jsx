32385e1688863cfa8ae44841c15e635786cd868a
lucia cappll <luciacappll@gmail.com>
2022-09-13 23:33:57 -0300

    Detail

---
+++ src/components/ItemDetail/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const ItemDetail = () => {
+  return (
+    <div>ItemDetail</div>
+  )
+}
+
+export default ItemDetail
---
+++ src/containers/ItemDetailContainer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const ItemDetailContainer = () => {
+  return (
+    <div>ItemDetailContainer</div>
+  )
+}
+
+export default ItemDetailContainer
