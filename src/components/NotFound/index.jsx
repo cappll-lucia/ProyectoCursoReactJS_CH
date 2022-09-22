4a3f33c702d643acc614fda5db6455a7e6f111af
lucia cappll <luciacappll@gmail.com>
2022-09-22 00:30:13 -0300

    NotFound

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const NotFound = () => {
+  return (
+    <div className='NotFound'>
+        <h2>Ups! Página no encontrada</h2>
+    </div>
+  )
+}
+
+export default NotFound
