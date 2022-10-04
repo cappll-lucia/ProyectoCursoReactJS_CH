97baaf20d425213a055c03ae30f2e159d8caa984
lucia cappll <luciacappll@gmail.com>
2022-10-04 09:59:00 -0300

    clearCart + removeCartItem

--- src/containers/CartContainer/index.jsx
+++ src/containers/CartContainer/index.jsx
@@ -3,45 +3,50 @@ import { useParams } from 'react-router-dom';
 import {Shop} from '../../context/ShopProvider';
 // import'../../../public/gallery/'
 import './styles.scss';
+import {BsFillTrashFill} from 'react-icons/bs';
+
 
 const CartContainer = () => {
 
   const {cart, removeItem, clearCart} =useContext(Shop);
-
+console.log( cart);
   return (
-    <div className='cartContainer'>
-      <div className="cartHeader">
-          <span className='headerImg'>IMG</span>
-          <span className='headerTitle'>title</span>
-          <span className='headerBrand'>brand</span>
-          <span className='headerCategory'>categ</span>
-          <span className='headerPrice'>precio</span>
-          <span className='headerQty'>cant</span>
-          <span className='headerTotal'>total</span>
+    <div className="cartContainer">
+      <div className='cart'>
+        <div className="cartHeader">
+            <span className='headerImg'>IMG</span>
+            <span className='headerTitle'>title</span>
+            <span className='headerBrand'>brand</span>
+            {/* <span className='headerCategory'>categ</span> */}
+            <span className='headerPrice'>precio</span>
+            <span className='headerQty'>cant</span>
+            <span className='headerTotal'>total</span>
+        </div>
+
+        {
+          cart.map(item =>{
+            return(
+              <div className="singleCartItem">
+                <div className="cartItemImg">
+                  <img src={`${item.img}`} alt="cartItemImg" />
+                </div>
+                <div className="cartItemInfo">
+                  <span className='cartItemTitle'>{item.title}</span>
+                  <span className='cartItemBrand'>{item.brand}</span>
+                  {/* <span className='cartItemCategory'>{item.categ}</span> */}
+                  <span className='cartItemPrice'>{item.price}</span>
+                  <span className='cartItemQty'>{item.quantity}</span>
+                  <span className='cartItemTotal'>{item.price*item.quantity}</span>
+                  <span onClick={()=>removeItem(item.id)} className='cartItemDeleteBtn'><BsFillTrashFill/></span>
+                </div>
+            </div>
+            )
+          })
+        }
       </div>
-
-      {
-        cart.map(item =>{
-          return(
-            <div className="singleCartItem">
-              <div className="cartItemImg">
-                <img src={`${item.img}`} alt="cartItemImg" />
-              </div>
-              <div className="cartItemInfo">
-                <span className='cartTitle'>{item.title}</span>
-                <span className='cartBrand'>{item.brand}</span>
-                <span className='cartCategory'>{item.categ}</span>
-                <span className='cartPrice'>{item.price}</span>
-                <span className='cartQty'>?</span>
-                <span className='cartTotal'>?</span>
-                <span className='cartDeleteBtn'><i class="fa-solid fa-trash"></i></span>
-              </div>
-          </div>
-          )
-        })
-      }
-
-
+      <div className="totalCart">
+      <button onClick={()=>clearCart()} className='clearCartBtn'>Vaciar Carrito<BsFillTrashFill/></button>
+      </div>
     </div>
   )
 }
