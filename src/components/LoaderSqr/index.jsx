0e052324c8fbbc48b6b10ffc7b25961341ddb58c
lucia cappll <luciacappll@gmail.com>
2022-10-06 09:49:23 -0300

    loaderSqr styles

--- src/components/LoaderSqr/index.jsx
+++ src/components/LoaderSqr/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './styles.scss';
+import  '../../App.scss';
 
 const LoaderSqr = ({messaje}) => {
   return (
-    <div className="loader">
-        <span>{messaje}</span>
-        <div className='outerCube cube'>
-            <div className="innerCube cube">
-                <div className="innerCube2 cube"></div>
-            </div>
-        </div>
-    </div>
+    <div className="screenSuspend">
+      <div className="loader">
+          <div className='outerCube cube'>
+              <div className="innerCube cube">
+                  <div className="innerCube2 cube"></div>
+              </div>
+          </div>
+          <span>{messaje}</span>
+      </div>
+      </div>
   )
 }
 
